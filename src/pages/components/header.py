e6be50d595a4c292e3e69b7b0d60ea9674ad11d6
Hein-Carl Frijlink <hc.frijlink@freedom.nl>
2023-10-12 08:57:24 +0200

    commit tests

--- src/pages/components/header.py
+++ src/pages/components/header.py
@@ -1,7 +1,7 @@
 class HeaderComponent:
     def __init__(self, page):
         self.page = page
-        self.login_btn = page.locator('a[class^="Buttonsstyles"][href="/login"]')
+        self.login_btn = page.locator('a[class^="Buttonsstyles"][href*="/login"]')
         self.member_info_btn = page.locator('[data-testid="header-member-menu-button"]')
         self.member_info_logout_btn = page.locator('[data-testid="account-menu-logout"]')
         self.logout_submit_btn = page.locator('button#logout-submit')
